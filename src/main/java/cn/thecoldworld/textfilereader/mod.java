7defd97255e53be03d799e7f58a6978a50a78936
TheColdWorld <3264524796@qq.com>
2023-10-28 23:22:18 +0800

    Release 1.0.8 :Specify the number of lines in the file to read after specifying the player (added in the next version without specifying the player); added a new condition for detecting yourself to avoid using 'The command executor is xxx' on yourself; migrated the code for loading events and synchronizing to files to a new static function; removed an unnecessary Log; added a GUI that is still under development; added a new hotkey to open the GUI; added a new hotkey to open the GUI; added a new hotkey to open the GUI. added a GUI that is still under development; added a new hotkey to open the GUI.

--- src/main/java/cn/thecoldworld/textfilereader/mod.java
+++ src/main/java/cn/thecoldworld/textfilereader/mod.java
@@ -3,11 +3,10 @@ package cn.thecoldworld.textfilereader;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerWorldEvents;
-import net.minecraft.util.WorldSavePath;
+import net.minecraft.util.Identifier;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -15,7 +14,6 @@ import java.util.concurrent.TimeUnit;
 public class mod implements ModInitializer {
     @Override
     public void onInitialize() {
-        variables.Log.info("Start Initialize TextFileReader mod");
         FileIO.Rootdir = Paths.get(System.getProperty("user.dir")).toAbsolutePath();
         if ( !FileIO.Rootdir.isAbsolute() ) throw new RuntimeException("Cannot get Data path");
         FileIO.GlobalTextPath = Paths.get(FileIO.Rootdir.toString(), "Texts");
@@ -37,49 +35,11 @@ public class mod implements ModInitializer {
         }
         FilePermissions.GlobalTextPermission = FilePermissions.InitPermission(Paths.get(FileIO.GlobalTextPath.toString(), "permissions.json"));
         variables.threadPool=new ThreadPool();
-        ServerWorldEvents.LOAD.register(((server, world) -> {
-            Path PermissionPath = server.getSavePath(WorldSavePath.ROOT).toAbsolutePath().resolve("Texts").resolve("permissions.json").normalize();
-            if ( !PermissionPath.getParent().normalize().toFile().exists() || !PermissionPath.getParent().normalize().toFile().isDirectory() ) {
-                try {
-                    funcitons.CreateDir(PermissionPath.getParent().normalize());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                } catch (Exception e) {
-                    if(!e.getMessage().equals("Dir exist")) throw new RuntimeException(e);
-                }
-            }
-            if ( !PermissionPath.toFile().exists() || !PermissionPath.toFile().isFile() ) {
-                variables.Log.info("Missing world text data permission file,starting crate");
-                try {
-                    funcitons.CreateFile(PermissionPath.toFile(),"{\"Files\":[]}");
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                } catch (Exception e) {
-                    if(!e.getMessage().equals("File exist")) throw new RuntimeException(e);
-                }
-            }
-            FilePermissions.WorldTextPermission = FilePermissions.InitPermission(PermissionPath);
-            variables.IsWorldLoaded=true;
-        }));
+        ServerWorldEvents.LOAD.register((funcitons::OnWorldLoading));
         variables.scheduledExecutorService = Executors.newSingleThreadScheduledExecutor(variables.threadPool);
-        variables.scheduledExecutorService.scheduleAtFixedRate(() -> {
-            if(!variables.IsWorldLoaded)return;
-            try {
-                if ( variables.TickEvent.isEmpty() ) {
-                    FilePermissions.GlobalTextPermission.UpdateFile();
-                    FilePermissions.WorldTextPermission.UpdateFile();
-                    FilePermissions.GlobalTextPermission.UpToFile();
-                    FilePermissions.WorldTextPermission.UpToFile();
-                    variables.ModSettings.UptoFile();
-                    return;
-                }
-                variables.TickEvent.take().run();
-            } catch (Exception e)
-            {
-                variables.Log.error("",e);
-            }
-        },0,500, TimeUnit.MICROSECONDS);
+        variables.scheduledExecutorService.scheduleAtFixedRate(funcitons::OnUpdateThread,0,500, TimeUnit.MICROSECONDS);
         ServerWorldEvents.UNLOAD.register(((server, world) -> variables.IsWorldLoaded=false));
-        variables.Log.info("Initialize TextFileReader mod done");
+        variables.TextFileIdentifier_get=new Identifier("textfilereader","transfer/textfile/get");
+        variables.TextFileIdentifier_receive=new Identifier("textfilereader","transfer/textfile/receive");
     }
 }
